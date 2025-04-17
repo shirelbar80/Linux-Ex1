2806f335e14cb580b99e6498ac9ea651dce334b4
Shirel Bar Lev <shirel.sam@gmail.com>
2025-04-17 16:48:44 +0300

    Question 1 completed
    shirel

---
+++ q1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // Open the file and check if it is open
+    ifstream file("/home/shirelbar80/Documents/Linux-Ex1/blocks.txt");
+    if (!file.is_open()) {
+        cerr << "Error: Could not open the file." << std::endl;
+        return 1;
+    }
+
+    string line;
+    int currentLine = 0;
+
+
+    while (getline(file, line)) {
+        currentLine++;
+       
+        if (currentLine % 7 == 0){//between two blocks
+
+            getline(file, line); // Read the next line
+
+            if(line[0]== 'h'){//not the end
+                cout << "|\n|\nV\n";
+                cout << line << std::endl;
+                currentLine++;
+            }
+            
+        }
+        else{
+            cout << line << std::endl;
+        }
+        
+    }
+
+
+    file.close();
+    return 0;
+}
